d5b6af689c5b8b30d858c729658de46b34bf33b4
marco <a20193265@pucp.edu.pe>
2025-05-18 15:49:50 -0500

    OLA

--- src/main/java/com/example/project/repository/superadmin/SuperAdminRepository.java
+++ src/main/java/com/example/project/repository/superadmin/SuperAdminRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface SuperAdminRepository extends JpaRepository<Usuarios, Integer> {
-    @Query(value="select count(*) from espacio where idEstadoEspacio= ?1",nativeQuery=true)
+    @Query(value="select count(*) from Espacio where idEstadoEspacio= ?1",nativeQuery=true)
     int cantidadDeEspaciosPorIdEstado(int idEstadoEspacio);
     @Transactional
     @Modifying
-    @Query(value="update usuarios set estado=?2 where idUsuarios=?1;",nativeQuery = true)
+    @Query(value="update Usuarios set estado=?2 where idUsuarios=?1;",nativeQuery = true)
     void actualizarEstadoUsuario(int idUsuario,int estado);
 }
